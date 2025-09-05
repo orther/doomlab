ce997d1489a957ef18159a74fb9eb9e8b3a73432
Brandon Orther <brandon@orther.dev>
2025-09-05 12:31:45 -0700

    Revert "Restore proper VM configuration with root password"
    This reverts commit cd1cba36cbea192ce23034540f4b6ff8641d3f2c.

--- machines/vm/configuration.nix
+++ machines/vm/configuration.nix
@@ -1,50 +1,57 @@
 
 {
-  config,
-  lib,
-  pkgs,
   inputs,
   outputs,
   ...
 }: {
   imports = [
-    ./hardware-configuration.nix
-    ../../modules/nixos/base.nix
+    inputs.impermanence.nixosModules.impermanence
     inputs.home-manager.nixosModules.home-manager
+
+    ./hardware-configuration.nix
+
+    ./../../modules/nixos/base.nix
+    ./../../modules/nixos/remote-unlock.nix
+    ./../../modules/nixos/auto-update.nix
+
+    # ./../../services/tailscale.nix
   ];
 
-  # System configuration
-  networking.hostName = "doomlab-vm";
-  time.timeZone = "America/Los_Angeles";
-
-  # User configuration
-  users.users.orther = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager"];
-    openssh.authorizedKeys.keys = [
-      # Add your SSH public key here
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDvJx1pyQwQVPPdXlqhJEtUlKyVr4HbZvgbjZ96t75Re brandon@orther.dev"
-    ];
-  };
-
-  # Enable root user with password for VM management
-  users.users.root = {
-    # Password: nixos123
-    hashedPassword = "$6$rounds=4096$salt$6yVUHsPblURs";
-  };
-
-  # SSH configuration
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
-    settings.PermitRootLogin = "yes";
-  };
-
-  # Home Manager
   home-manager = {
     extraSpecialArgs = {inherit inputs outputs;};
-    users.orther = import ../../modules/home-manager/nixos.nix;
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      orther = {
+        imports = [
+          ./../../modules/home-manager/base.nix
+        ];
+
+        programs.git = {
+          enable = true;
+          userName = "Brandon Orther";
+          userEmail = "brandon@orther.dev";
+          # Signing config remains the same if needed
+        };
+
+        programs.ssh = {
+          enable = true;
+          matchBlocks = {
+            "github.com" = {
+              hostname = "github.com";
+              identityFile = "~/.ssh/id_ed25519";
+              user = "git";
+            };
+          };
+        };
+      };
+    };
   };
 
-  system.stateVersion = "24.11";
+  networking = {
+    hostName = "vm";
+    useDHCP = false;
+    interfaces.enp1s0.useDHCP = true;
+    useNetworkd = true;
+  };
 }
